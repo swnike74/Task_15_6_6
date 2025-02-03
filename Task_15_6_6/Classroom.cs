98e0967dc6c8be2b3017c0e07019b6134f908cae
Ведерников Сергей <swnike@mail.ru>
2025-02-03 21:19:29 +0500

    Добавьте файлы проекта.

---
+++ Task_15_6_6/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_15_6_6
+{
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Task_15_6_6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_15_6_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            // ???
+        }
+    }
+    }
+}
